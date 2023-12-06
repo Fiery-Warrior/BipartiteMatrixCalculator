8f3add30399e9943ef0bdc2ed1adc4bdb03480d4
Keagan Bogart <keagan.j.bogart@gmail.com>
2023-12-05 21:52:50 -0600

    Bipartite bare matrix

---
+++ bip-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Landing from './Landing/Landing';
+import Isa from './Isa/Isa';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+         
+        <Landing/>
+   
+        <Isa/>
+
+      </header>
+
+      <footer className="footer">
+
+        <p>Copyright © Keagan Bogart 2023</p>
+    </footer>
+    </div>
+  );
+}
+
+
+
+export default App;
